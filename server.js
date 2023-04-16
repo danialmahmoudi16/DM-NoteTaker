450d9553f7e4f9cfb12f70964960e3f8bb1de69d
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-04-16 15:23:27 -0400

    created the dependencies that are to be used for the application.

---
+++ server.js
@@ -0,0 +1,24 @@
+// Brings in express from node.js
+const express = require('express');
+
+// brings in node.js fs module
+const fs = require('fs');
+
+// allows to extract contents from module.exports
+const path = require('path');
+
+// brings in uuid method to make unique id for each note input
+const uuid = require('uuid');
+
+// tells server to listen to the specific port 3001
+const PORT = process.nextTick.PORT || 3001;
+
+// creates express application
+const app = express;
+
+// middleware for parsing JSON and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
